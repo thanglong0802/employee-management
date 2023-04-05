04c06d8bbcb5f12b24cc30538d105e8b01d571aa
thanglong0802 <thanglong08022k@gmail.com>
2023-04-05 17:20:48 +0700

    add put delete rest api

--- employee-management-system/src/main/java/com/longnt/employeemanagementsystem/controller/EmployeeController.java
+++ employee-management-system/src/main/java/com/longnt/employeemanagementsystem/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.longnt.employeemanagementsystem.controller;
 
+import com.longnt.employeemanagementsystem.exception.ResourceNotFoundException;
 import com.longnt.employeemanagementsystem.model.Employee;
 import com.longnt.employeemanagementsystem.repository.EmployeeRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,5 +34,34 @@ public class EmployeeController {
     @GetMapping("/employees/{id}")
     public Employee getEmployeeById(@PathVariable("id") Long id) {
         return employeeRepository.findEmployeeById(id);
+    }
+
+    // update employee by id
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployeeById(@PathVariable("id") Long id, @RequestBody Employee employee) {
+//        Employee employeeUpdate = employeeRepository.findEmployeeById(id);
+//
+//        employeeUpdate.setFirstName(employee.getFirstName());
+//        employeeUpdate.setLastName(employee.getLastName());
+//        employeeUpdate.setEmailId(employee.getEmailId());
+//
+//        Employee update = employeeRepository.save(employeeUpdate);
+//        return ResponseEntity.ok(update);
+
+        return employeeRepository.findById(id)
+                .map(employee1 -> {
+                    employee1.setFirstName(employee.getFirstName());
+                    employee1.setLastName(employee.getLastName());
+                    employee1.setEmailId(employee.getEmailId());
+                    return ResponseEntity.ok(employee1);
+                }).orElseGet(() ->
+                    ResponseEntity.ok(employeeRepository.save(employee))
+                );
+    }
+
+    // delete employee by id
+    @DeleteMapping("/employees/{id}")
+    public void deleteEmployeeById(@PathVariable("id") Long id) {
+        employeeRepository.deleteById(id);
     }
 }
--- employee-management-system/src/main/java/com/longnt/employeemanagementsystem/repository/EmployeeRepository.java
+++ employee-management-system/src/main/java/com/longnt/employeemanagementsystem/repository/EmployeeRepository.java
@@ -4,9 +4,8 @@ import com.longnt.employeemanagementsystem.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Employee findEmployeeById(Long id);
+    void deleteById(Long id);
 }
